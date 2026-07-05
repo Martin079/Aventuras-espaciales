b2daa28fb319b231dc3361febb0056b84236e67c
Anza Míguez Axel <axelanzamiguez@gmail.com>
2026-07-04 23:53:23 -0300

    Override mostrarRecursosDisponibles in Gaseoso class

--- Aventuras espaciales/src/planetas/Gaseoso.java
+++ Aventuras espaciales/src/planetas/Gaseoso.java
@@ -5,6 +5,14 @@ import utilidades.Aleatorio;
 
 public class Gaseoso extends Planeta{
 
+		@Override
+    public void mostrarRecursosDisponibles() {
+        System.out.println("Recursos disponibles en el planeta gaseoso:");
+        System.out.println("Gas: 60%");
+        System.out.println("Plasma: 25%");
+        System.out.println("Cristal: 15%");
+    }
+
 	public Gaseoso(){
 		super("Gaseoso");
 	}
